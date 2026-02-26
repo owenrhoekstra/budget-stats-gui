fb7c6d52f850ed7079f998525e93f037061719df
owenrhoekstra <owenhoekstra@icloud.com>
2026-02-26 15:23:11 -0500

    git repo created

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ElButton as Button } from 'element-plus'
+import { ref } from 'vue'
+
+const click = ref(0)
+
+function handleClick() {
+  console.log('click')
+  click.value++
+}
+</script>
+
+<template>
+  <Button @click="handleClick">
+    Click Me
+  </Button>
+  <p>{{click}}</p>
+</template>
+
+<style scoped>
+
+</style>
